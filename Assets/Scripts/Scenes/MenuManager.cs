74ef58a92af9ecc2ef6a7776dcdb05670770e45b
franceko <forzaroma61@mail.ru>
2026-02-17 16:18:14 +0300

    bug fix

--- Assets/Scripts/Scenes/MenuManager.cs
+++ Assets/Scripts/Scenes/MenuManager.cs
@@ -4,11 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public Texture2D cursorTexture;
-    public Vector2 hotspot = Vector2.zero;
+    [SerializeField] private Texture2D cursorTexture;    
+    [SerializeField] private Vector2 hotspot = Vector2.zero;
+
     private void Start()
     {
+        ApplyCustomCursor();
+    }
+
+    private void ApplyCustomCursor()
+    {
+        if (cursorTexture == null)
+        {
+            Cursor.visible = true;
+            return;
+        }
+
+#if UNITY_WEBGL
+        Cursor.SetCursor(cursorTexture, hotspot, CursorMode.ForceSoftware);
+#else
         Cursor.SetCursor(cursorTexture, hotspot, CursorMode.Auto);
+#endif
+
+        Cursor.visible = true;  
+    }
+
+    public void RefreshCursor()
+    {
+        ApplyCustomCursor();
     }
 
     public void onPlayButtonClick() => SceneManager.LoadScene("MainScene");
